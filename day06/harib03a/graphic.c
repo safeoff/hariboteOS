dbb30a5e0cf31923d34d5ca46da899d52c198750
ry <sgmrrc@gmail.com>
2018-06-14 21:44:18 +0900

    day06/harib03a

--- day06/harib03a/graphic.c
+++ day06/harib03a/graphic.c
@@ -1,5 +1,3 @@
-#include<stdio.h>
-
 void io_hlt(void);
 void io_cli(void);
 void io_out8(int port, int data);
@@ -32,54 +30,6 @@ void putblock8_8(char *vram, int vxsize, int pxsize,
 #define COL8_840084		13
 #define COL8_008484		14
 #define COL8_848484		15
-
-struct BOOTINFO 
-{
-    char cyls, leds, vmode, reserve;
-    short scrnx, scrny;
-    char *vram;    
-};
-
-struct SEGMENT_DESCRIPTOR 
-{
-    short limit_low, base_low;
-	char base_mid, access_right;
-	char limit_high, base_high;
-};
-
-struct GATE_DESCRIPTOR {
-	short offset_low, selector;
-	char dw_count, access_right;
-	short offset_high;
-};
-
-void init_gdtidt(void);
-void set_segmdesc(struct SEGMENT_DESCRIPTOR *sd, unsigned int limit, int base, int ar);
-void set_gatedesc(struct GATE_DESCRIPTOR *gd, int offset, int selector, int ar);
-void load_gdtr(int limit, int addr);
-void load_idtr(int limit, int addr);
-
-void HariMain(void)
-{
-    struct BOOTINFO *binfo = (struct BOOTINFO *) 0x0ff0;
-    char s[40], mcursor[256];
-    int mx, my;
-
-    init_gdtidt();
-    init_palette();
-    init_screen(binfo->vram, binfo->scrnx, binfo->scrny);
-	mx = (binfo->scrnx - 16) / 2; /* 画面中央になるように座標計算 */
-	my = (binfo->scrny - 28 - 16) / 2;
-	init_mouse_cursor8(mcursor, COL8_008484);
-	putblock8_8(binfo->vram, binfo->scrnx, 16, 16, mx, my, mcursor, 16);
-	lsprintf(s, "(%d, %d)", mx, my);
-	putfonts8_asc(binfo->vram, binfo->scrnx, 0, 0, COL8_FFFFFF, s);
-
-    for(;;)
-    {
-        io_hlt();
-    }
-}
 
 void init_palette(void)
 {
@@ -236,52 +186,5 @@ void putblock8_8(char *vram, int vxsize, int pxsize,
 			vram[(py0 + y) * vxsize + (px0 + x)] = buf[y * bxsize + x];
 		}
 	}
-	return;
-}
-
-void init_gdtidt(void)
-{
-    struct SEGMENT_DESCRIPTOR *gdt = (struct SEGMENT_DESCRIPTOR *) 0x00270000;
-    struct GATE_DESCRIPTOR    *idt = (struct GATE_DESCRIPTOR    *) 0x0026f800;
-    int i;
-
-    for (i = 0; i < 8192; i++)
-    {
-        set_segmdesc(gdt + i, 0, 0, 0);
-    }
-	set_segmdesc(gdt + 1, 0xffffffff, 0x00000000, 0x4092);
-	set_segmdesc(gdt + 2, 0x0007ffff, 0x00280000, 0x409a);
-	load_gdtr(0xffff, 0x00270000);
-
-	for (i = 0; i < 256; i++) {
-		set_gatedesc(idt + i, 0, 0, 0);
-	}
-	load_idtr(0x7ff, 0x0026f800);
-
-	return;
-}
-
-void set_segmdesc(struct SEGMENT_DESCRIPTOR *sd, unsigned int limit, int base, int ar)
-{
-	if (limit > 0xfffff) {
-		ar |= 0x8000;
-		limit /= 0x1000;
-	}
-	sd->limit_low    = limit & 0xffff;
-	sd->base_low     = base & 0xffff;
-	sd->base_mid     = (base >> 16) & 0xff;
-	sd->access_right = ar & 0xff;
-	sd->limit_high   = ((limit >> 16) & 0x0f) | ((ar >> 8) & 0xf0);
-	sd->base_high    = (base >> 24) & 0xff;
-	return;
-}
-
-void set_gatedesc(struct GATE_DESCRIPTOR *gd, int offset, int selector, int ar)
-{
-	gd->offset_low   = offset & 0xffff;
-	gd->selector     = selector;
-	gd->dw_count     = (ar >> 8) & 0xff;
-	gd->access_right = ar & 0xff;
-	gd->offset_high  = (offset >> 16) & 0xffff;
 	return;
 }
